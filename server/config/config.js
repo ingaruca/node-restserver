0787b5701bb2a576577240595d5bd1310837c926
Luis Ingaruca <luise.ingaruca@gmail.com>
2018-10-01 14:38:06 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:cafe1992@ds137246.mlab.com:37246/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
