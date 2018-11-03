af3f719a44d3f6b6d15bf2b013c563f63410d73f
Luis Ingaruca <luise.ingaruca@gmail.com>
2018-11-02 23:53:05 -0500

    Login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -5,11 +5,12 @@ const bcrypt = require('bcrypt')
 const _ = require('underscore')
 
 const Usuario = require('../models/usuario')
+const {verificaToken, verificarAdmin_Role} = require('../middlewares/autenticacion')
 
 const app = express()
 
 //GET
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificaToken ,(req, res) => {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -42,7 +43,7 @@ app.get('/usuario', function (req, res) {
 })
   
 //POST
-app.post('/usuario', function (req, res) {
+app.post('/usuario', [verificaToken, verificarAdmin_Role], function (req, res) {
 
     let body = req.body
 
@@ -75,7 +76,7 @@ app.post('/usuario', function (req, res) {
 })
 
 //PUT
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id', [verificaToken, verificarAdmin_Role], function (req, res) {
 
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
@@ -98,7 +99,7 @@ app.put('/usuario/:id', function (req, res) {
 })
 
 //DELETE
-app.delete('/usuario/:id', function (req, res) {
+app.delete('/usuario/:id', [verificaToken, verificarAdmin_Role], function (req, res) {
 
     let id = req.params.id;
     let cambiaEstado = {
